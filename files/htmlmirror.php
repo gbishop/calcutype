84193419acd6b485d82bfe631da7872fe3a49f14
condonwe <condonwe@4eff19f8-dc25-11dd-a9d2-799639727c04>
2009-01-07 02:11:28 +0000

    added PHP file to echo HTML forms - still working on code for saving forms.

---
+++ files/htmlmirror.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<?php
+//(By the way, this should be a comment.)
+//We're mirroring an HTML submission from a dummy form generated specifically for this purpose.
+//Unfortunately, I'm forced to use HTML 4.01 Transitional.
+echo $_POST["allHTML"];
+?>
+</html>
