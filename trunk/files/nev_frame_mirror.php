fd7477d3f765d7fe44e0ae1bbd24dd5ec72bc8e1
condonwe <condonwe@4eff19f8-dc25-11dd-a9d2-799639727c04>
2009-02-06 00:09:46 +0000

    Fix? bug with PHP program

--- trunk/files/nev_frame_mirror.php
+++ trunk/files/nev_frame_mirror.php
@@ -8,7 +8,7 @@ function isAllowedExtension($fileName) {
 }
 
 if($file['error'] == UPLOAD_ERR_OK && isAllowedExtension($file['name'])) {
-  echo file_get_contents($_FILES["form"]);
+  echo file_get_contents($_FILES["form"]["tmp_name"]);
 }
 else {
   echo "Cannot upload.  Either there was a server error or you provided an invalid file type.  Valid file types include <tt>*.html</tt>, <tt>*.htm</tt>, and <tt>*.txt</tt>.";
